dc185706d172d161e7c6f7eac5bdb27c7142317d
william <factzure@hotmail.com>
2021-09-10 21:40:53 -0500

    Agregar archivos de proyecto.

---
+++ projecto20_NumeroPar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecto20_NumeroPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un programa que que me indique si un numero es par o impar, 
+            //que muestre un  mensaje de salida
+
+            int numero;
+            Console.WriteLine("Ingrese el Numero ? ");
+            numero = int.Parse(Console.ReadLine());
+            if (numero % 2 ==0)
+            {
+                Console.WriteLine("el numero es Par ");
+            }
+            else
+            {
+                Console.WriteLine("El numero es impar ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
